d413747174f243f93b4b244172c7e5b6c9267c3e
Lucas Keiti <lucaskeiti2010@gmail.com>
2022-06-08 23:28:53 -0300

    Updated index.html, stylization some properties, and responsiveness experience bar

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//estado, componente e propriedade
+
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css';
+
+function App() {
+  return (
+   <div className="container">
+     <ExperienceBar />
+   </div>
+  );
+}
+
+export default App;
